acfea94c2d01538e9e688b46add3db7796e20989
Sophia Lee <sophia.anna.lee@gmail.com>
2023-03-10 15:15:41 -0500

    Added requirements.txt and began to correct box_overlaps.c

--- crop_data/train_val_split.py
+++ crop_data/train_val_split.py
@@ -6,11 +6,13 @@ lines_train = [line.rstrip('\n') for line in open('train.txt')]
 lines_val = [line.rstrip('\n') for line in open('val.txt')]
 
 for i in lines_train:
+  os.remove('training/calib/'+i+'.txt')
   os.remove('training/image_2/'+i+'.png')
   os.remove('training/label_2/'+i+'.txt')
   os.remove('training/velodyne/'+i+'.bin')
 
 for i in lines_val:
+  os.remove('validation/calib/'+i+'.txt')
   os.remove('validation/image_2/'+i+'.png')
   os.remove('validation/label_2/'+i+'.txt')
   os.remove('validation/velodyne/'+i+'.bin')
